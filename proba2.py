d08ee6dbc7dab79a2de50e280e18635fc28919ee
Sveta-ai <stralev@mail.ru>
2024-08-29 20:40:58 +0300

    Домашнее заданиеБазовые структуры данных

---
+++ proba2.py
@@ -0,0 +1,16 @@
+print(float(9)) # "1st program"
+print(9.0**0.5)
+print(3.0*5)
+print(9.99>9.98 , 1000!=1000.1) # "2st program"
+print(2*2+2) # "3st program" без приоритета
+print(2*(2+2)) # с приоритетом для сложения
+print(6==8) # сравнение результатов
+print(float("123.456")) # "4st program"
+print(123.456*10)
+print(int(1234.56))
+print(1234%10)
+
+
+
+
+
